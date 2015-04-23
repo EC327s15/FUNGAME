9421ce7e87e6bfec0d6b3bcfdf37fae9887bd66d
Yigang Wang <ico.yigang@hotmail.com>
2015-04-23 06:21:00 -0700

    Update FishPanel.java

--- FishPanel.java
+++ FishPanel.java
@@ -18,12 +18,17 @@ public class FishPanel extends JPanel
 		g.fillRect(0, 0, 800, 700); 
 		MainFish mainFish = MainFish.mainFish;
 		OtherFish otherFish = OtherFish.otherFish;
-	
+
 		g.setColor(Color.blue);
 		g.fillOval((int)mainFish.fish.x * MainFish.SCALE, (int)mainFish.fish.y * MainFish.SCALE, (int)mainFish.fishR * MainFish.SCALE, (int)mainFish.fishR * MainFish.SCALE);
 
 		g.setColor(Color.BLACK);
 		g.fillOval((int)otherFish.oFish.x * MainFish.SCALE, (int)otherFish.oFish.y * MainFish.SCALE, (int)otherFish.oFishR * MainFish.SCALE, (int)otherFish.oFishR * MainFish.SCALE);
-
+		if(MainFish.over)
+		{
+			String gameOver = "Game Over! Your are so delicious!";
+			g.setColor(Color.RED);
+			g.drawString(gameOver, (int)(getWidth() / 2 - gameOver.length() * 2.5f), 50);
+		}
 	}
 }
