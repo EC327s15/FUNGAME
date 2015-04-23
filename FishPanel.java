b4f95b9664826b01e81503093a361b67cef5d0d9
Yigang Wang <ico.yigang@hotmail.com>
2015-04-23 07:40:04 -0700

    Update FishPanel.java

--- FishPanel.java
+++ FishPanel.java
@@ -18,17 +18,33 @@ public class FishPanel extends JPanel
 		g.fillRect(0, 0, 800, 700); 
 		MainFish mainFish = MainFish.mainFish;
 		OtherFish otherFish = OtherFish.otherFish;
-
 		g.setColor(Color.blue);
 		g.fillOval((int)mainFish.fish.x * MainFish.SCALE, (int)mainFish.fish.y * MainFish.SCALE, (int)mainFish.fishR * MainFish.SCALE, (int)mainFish.fishR * MainFish.SCALE);
 
 		g.setColor(Color.BLACK);
 		g.fillOval((int)otherFish.oFish.x * MainFish.SCALE, (int)otherFish.oFish.y * MainFish.SCALE, (int)otherFish.oFishR * MainFish.SCALE, (int)otherFish.oFishR * MainFish.SCALE);
-		if(MainFish.over)
+		String record = "Score: " + MainFish.score;
+		g.setColor(Color.RED);
+		g.drawString(record, (int) (getWidth() / 2 - record.length() * 2.5f), 10);
+		if(MainFish.over && OtherFish.eatOtherFish())
 		{
 			String gameOver = "Game Over! Your are so delicious!";
-			g.setColor(Color.RED);
-			g.drawString(gameOver, (int)(getWidth() / 2 - gameOver.length() * 2.5f), 50);
+			g.setColor(Color.black);
+			g.drawString(gameOver, (int)(getWidth() / 2 - gameOver.length() * 2.5f), 80);
+		}
+		
+		else if(MainFish.over && MainFish.fishR <= 2)
+		{
+			String gameOver = "Game Over! I'am hungry to die!";
+			g.setColor(Color.black);
+			g.drawString(gameOver, (int)(getWidth() / 2 - gameOver.length() * 2.5f), 80);
+		}
+		
+		else if(MainFish.over && MainFish.fishR >= 10)
+		{
+			String gameOver = "Game Over! You feed me too much bro!";
+			g.setColor(Color.black);
+			g.drawString(gameOver, (int)(getWidth() / 2 - gameOver.length() * 2.5f), 80);
 		}
 	}
 }
