c2f64fe40b76a4750f4448fb2657a486ce037de2
Yigang Wang <ico.yigang@hotmail.com>
2015-04-23 20:38:32 -0700

    Update OtherFish.java

--- OtherFish.java
+++ OtherFish.java
@@ -12,8 +12,8 @@ public class OtherFish
 	public OtherFish(Double oFish, double oSpeed, double oFishR)
 	{
 		this.oFish = oFish;
-		double p = (int) Math.ceil(Math.random()*85);
-		oFish.x = p;
+		//double p = (int) Math.ceil(Math.random()*80);
+		//oFish.x = p;
 		this.oSpeed = oSpeed;
 		double q = (int) Math.ceil(Math.random()*8);
 		oFishR = q;
@@ -22,7 +22,44 @@ public class OtherFish
 
 	public void otherFishMove()
 	{
-		oFish.y += oSpeed;
+		if(MainFish.level == 1)
+			oFish.y += oSpeed;
+		
+		else if(MainFish.level == 2)
+			oFish.x += oSpeed;
+		
+		else if(MainFish.level == 3)
+		{
+			if(MainFish.numSmallFish % 2 == 0)
+			{
+				oFish.y += oSpeed;
+			}
+			if(MainFish.numSmallFish % 2 == 1)
+			{
+				oFish.x += oSpeed;
+			}
+		}
+		
+		else if(MainFish.level == 4)
+		{
+			if(MainFish.numSmallFish % 4 == 0)
+			{
+				oFish.y += oSpeed;
+			}
+			else if(MainFish.numSmallFish % 4 == 1)
+			{
+				oFish.x += oSpeed;
+			}
+			else if(MainFish.numSmallFish % 4 == 2)
+			{
+				oFish.y -= oSpeed;
+			}
+			else if(MainFish.numSmallFish % 4 == 3)
+			{
+				oFish.x -= oSpeed;
+			}
+		}
+			
 	}
 	
 	public static boolean eatOtherFish()
@@ -30,7 +67,6 @@ public class OtherFish
 		if(Math.pow(oFishR + MainFish.mainFish.getFishR(), 2) > Math.pow(MainFish.mainFish.getFishX() - oFish.x, 2) + Math.pow(MainFish.mainFish.getFishY() - oFish.y, 2))
 		{
 			MainFish.mainFish.eaten = true;
-			MainFish.score++;
 			return true;
 		}
 		else
@@ -42,7 +78,55 @@ public class OtherFish
 	
 	public static OtherFish generateOtherFish()
 	{
-		otherFish = new OtherFish(new Point2D.Double(50, 1.0), 1.0, 3.0);
+		if(MainFish.level == 1)
+		{
+			otherFish = new OtherFish(new Point2D.Double(Math.ceil(Math.random()*80), 1.0), 1.0, 3.0);
+			return otherFish;
+		}
+		
+		else if(MainFish.level == 2)
+		{
+			otherFish = new OtherFish(new Point2D.Double(1, Math.ceil(Math.random()*65)), 1.0, 3.0);
+			return otherFish;
+		}
+		
+		else if(MainFish.level == 3)
+		{
+			if(MainFish.numSmallFish % 2 == 0)
+			{
+				otherFish = new OtherFish(new Point2D.Double(1, Math.ceil(Math.random()*65)), 1.0, 3.0);
+				return otherFish;
+			}
+			else if(MainFish.numSmallFish % 2 == 1)
+			{
+				otherFish = new OtherFish(new Point2D.Double(Math.ceil(Math.random()*80), 1.0), 1.0, 3.0);
+				return otherFish;
+			}
+		}
+		
+		else if(MainFish.level == 4)
+		{
+			if(MainFish.numSmallFish % 4 == 0)
+			{
+				otherFish = new OtherFish(new Point2D.Double(Math.ceil(Math.random()*80), 1), 1.0, 3.0);
+				return otherFish;
+			}
+			else if(MainFish.numSmallFish % 4 == 1)
+			{
+				otherFish = new OtherFish(new Point2D.Double(1, Math.ceil(Math.random()*65)), 1.0, 3.0);
+				return otherFish;
+			}
+			else if(MainFish.numSmallFish % 4 == 2)
+			{
+				otherFish = new OtherFish(new Point2D.Double(Math.ceil(Math.random()*80), 65), 1.0, 3.0);
+				return otherFish;
+			}
+			else if(MainFish.numSmallFish % 4 == 3)
+			{
+				otherFish = new OtherFish(new Point2D.Double(80, Math.ceil(Math.random()*65)), 1.0, 3.0);
+				return otherFish;
+			}
+		}
 		return otherFish;
 	}
 }
